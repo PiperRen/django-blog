72ed89b82c00b7d78e8491341e03ceaabcf2d09b
Lauren Turner <turnel16@gmail.com>
2023-01-28 15:24:59 -0800

    Finished List View

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
+from django.shortcuts import render
 
 
 # Create your views here.
 def stub_view(request, *args, **kwargs):
-    body = "Stub View\n"
+    body = "Stub View\n\n"
 
     if args:
         body += "Args:\n"
@@ -14,4 +17,11 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    # template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    # body = template.render(context)
+    # return HttpResponse(body, content_type='text/html')
+    return render(request, 'blogging/list.html', context)
